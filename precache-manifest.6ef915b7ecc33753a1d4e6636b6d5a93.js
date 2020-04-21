02ea4967e6fd79475414cae81cc05b0f199ab07a
Patrick Kille <pr.kille@gmail.com>
2020-04-21 15:54:55 -0700

    Updates

---
+++ precache-manifest.6ef915b7ecc33753a1d4e6636b6d5a93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6b9e09d0b62526d75d27b280873c4b",
+    "url": "/React-Farmers-Market/index.html"
+  },
+  {
+    "revision": "0d31da685b105cd3d3fd",
+    "url": "/React-Farmers-Market/static/css/main.6b26d62e.chunk.css"
+  },
+  {
+    "revision": "983981343b8053610d14",
+    "url": "/React-Farmers-Market/static/js/2.1a57be25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Farmers-Market/static/js/2.1a57be25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d31da685b105cd3d3fd",
+    "url": "/React-Farmers-Market/static/js/main.3cea4b00.chunk.js"
+  },
+  {
+    "revision": "691813e9c4740bea0355",
+    "url": "/React-Farmers-Market/static/js/runtime-main.177d3e90.js"
+  }
+]);
